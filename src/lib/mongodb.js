200d08d7aca9dbda359d4f655bac9fd723bcea63
Meera <meerakukadiya07@gmail.com>
2025-08-29 12:47:54 +0530

    update mongodb file

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,12 +1,31 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  if (mongoose.connection.readyState >= 1) return;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-  return mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-};
+if (!MONGODB_URI) {
+  throw new Error("⚠️ Please define the MONGODB_URI environment variable in Vercel");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
 
 export default connectDB;
